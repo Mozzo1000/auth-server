8c953fa1a2557025ee3242693cf92f96d7a6365d
Andreas Backström <andreas@simplymozzo.se>
2024-03-21 20:38:42 +0100

    Add auth services

---
+++ web/src/services/auth.service.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const register = (email, name, password) => {
+    return axios.post(API_URL + "/v1/register", {
+        email,
+        name,
+        password,
+    });
+};
+
+const login = (email, password) => {
+    return axios
+        .post(API_URL + "/v1/login", {
+            email,
+            password,
+        })
+        .then((response) => {
+            if (response.data.access_token) {
+                localStorage.setItem("auth_user", JSON.stringify(response.data));
+            }
+
+            return response.data;
+        });
+};
+
+const logout = () => {
+    /*TODO: Send logout request to auth-server so the token get invalidated. */
+    localStorage.removeItem("auth_user");
+};
+
+const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("auth_user"));
+};
+
+const verify = (email, code) => {
+    return axios.post(API_URL + "/v1/verify", {
+        email,
+        code,
+    });
+};
+
+export default {
+    register,
+    login,
+    logout,
+    getCurrentUser,
+    verify,
+};
